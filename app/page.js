e21ef00922c59d95ed4a0d1f87fafbc1fa27aa81
Your Name <you@example.com>
2024-02-23 18:52:54 -0500

    all data is Ok to display

--- app/page.js
+++ app/page.js
@@ -118,7 +118,7 @@ export default function Home() {
             </h2>
             <p>Please enable location in your browser settings.</p>
             <div
-              className="mt-4 px-4 py-2 bg-blue-300 text-white rounded hover:bg-blue-100 focus:outline-none focus:bg-blue-100"
+              className="mt-4 px-4 py-2 bg-blue-500 text-center text-white rounded hover:bg-blue-300 focus:outline-none focus:bg-blue-100"
               onClick={removealert}
             >
               Ok
